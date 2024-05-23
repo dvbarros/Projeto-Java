670b8a086ba8d245e8a4eca9e40323717607526f
Davi Barros <163138279+dvbarros@users.noreply.github.com>
2024-05-23 15:04:04 -0300

    Update Tarefa.java

--- Projeto-Java/Tarefa.java
+++ Projeto-Java/Tarefa.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Tarefa {
     public String nome;
     public int dia;
@@ -13,32 +11,6 @@ public class Tarefa {
         this.mes = mes;
         this.hora = hora;
         this.minuto = minuto;
-    }
-    
-    @Override
-    public String toString() {
-        return "Tarefa: " + nome + "\nData: " + dia + "/" + mes + "\nHorário: " + hora + ":" + minuto;
-    }
-
-    public void perguntas(){
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("\n\nDigite o nome da tarefa:");
-            nome = sc.nextLine();
-
-            System.out.println("Digite o dia da tarefa:");
-            dia = sc.nextInt();
-
-            System.out.println("Digite o mês da tarefa:");
-            mes = sc.nextInt();
-
-            System.out.println("Digite a hora da tarefa:");
-            hora = sc.nextInt();
-
-            System.out.println("Digite os minutos da tarefa:");
-            minuto = sc.nextInt();
-
-            sc.nextLine();
-        }
     }
     public void imprimirTarefa (){
         System.out.println("Tarefa: " + nome);
